30f513073f75d4a0a0df68e35caec36973235db6
h_bagheri <hassanbagheri233@gmail.com>
2021-05-01 08:34:51 +0430

    update

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
@@ -22,13 +24,17 @@ class LoginActivity : AppCompatActivity() {
 
         btn_login.setOnClickListener {
 
-
+            show_prgressBar()
             val email = edt_email.text.toString()
             val password = edt_password.text.toString()
+            Log.e("hassan" , email+"      "+password)
 
             if (email.isEmpty() || password.isEmpty()) {
+                hide_prgressBar()
                 toast("لظفا ایمیل و پسورد را وارد کنید ")
+
             } else
+                hide_prgressBar()
                 checkLogin(email, password)
 
 
@@ -43,12 +49,20 @@ class LoginActivity : AppCompatActivity() {
         firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
             if (it.isSuccessful) {
                 toast("با موفقعیت وارد شدید")
-                val getEmail = firebaseAuth.currentUser.email
-                val getUid = firebaseAuth.currentUser.uid
+//                val getEmail = firebaseAuth.currentUser.email
+//                val getUid = firebaseAuth.currentUser.uid
             } else
                 toast(it.exception?.message.toString())
         }
 
+    }
+
+    fun show_prgressBar(){
+        login_progressBar.visibility = View.VISIBLE
+    }
+
+    fun hide_prgressBar(){
+        login_progressBar.visibility = View.INVISIBLE
     }
 
 
