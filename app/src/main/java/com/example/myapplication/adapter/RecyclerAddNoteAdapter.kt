a9b3a8a52213cbbc952859132cea20c114527ed9
h_bagheri <hassanbagheri233@gmail.com>
2021-05-07 17:28:04 +0430

    write in datebase

---
+++ app/src/main/java/com/example/myapplication/adapter/RecyclerAddNoteAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.Data.Note
+import com.example.myapplication.R
+import kotlinx.android.synthetic.main.item_note_recyclerview.view.*
+
+
+class RecyclerNoteAdapter (private val data : List<Note>):
+    RecyclerView.Adapter<RecyclerNoteAdapter.ShopViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ShopViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_note_recyclerview,
+                    parent,
+                    false)
+        )
+
+    override fun onBindViewHolder(holder: ShopViewHolder, position: Int) {
+        holder.setData(data[position])
+    }
+
+    override fun getItemCount() = data.size
+
+
+    inner class ShopViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val date = itemView.txt_itemdate
+        private val desc = itemView.txt_itemdesc
+        private val title = itemView.txt_itemtitle
+
+        fun setData(data : Note){
+
+            date.text = data.date
+            desc.text = data.desc
+            title.text = data.titel
+
+        }
+    }
+}
